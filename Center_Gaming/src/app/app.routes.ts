fd997d97f275e80573a622a576e7137a920bf96c
maxenceferiau <oursa59170@gmail.com>
2024-04-15 15:54:29 +0200

    accueil debut

--- Center_Gaming/src/app/app.routes.ts
+++ Center_Gaming/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import PageAcceuilComponent from './page-acceuil/page-acceuil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "" , component : PageAcceuilComponent}
+];
--- Center_Gaming/src/app/page-acceuil/page-acceuil.component.ts
+++ Center_Gaming/src/app/page-acceuil/page-acceuil.component.ts
@@ -7,6 +7,60 @@ import { Component } from '@angular/core';
   templateUrl: './page-acceuil.component.html',
   styleUrl: './page-acceuil.component.css'
 })
-export class PageAcceuilComponent {
+export default class PageAcceuilComponent {
+  listeJeux = JEU
+  mouseenter(){
+  
+    const video = document.querySelector(".vid") as HTMLVideoElement;
+    const photo = document.querySelector(".phot") as HTMLElement;
+    
+    if (video && photo) {
+      photo.style.opacity = "0";
+      video.play();
+    } else {
+      console.error("Error:!");
+    }
+  }
+    mouseleave()
+    {
+      const video = document.querySelector(".vid") as HTMLVideoElement;
+      const photo = document.querySelector(".phot") as HTMLElement;
+      photo.style.opacity= "";
+      video.pause();
+    }
 
+  
+
+
+     photoJeux() {
+      
+   const jeuxdumoment = document.querySelector(".carteJeux"); 
+
+ for (let i = 0; i < 9; i++) {
+   const div = document.createElement('div');
+   div.classList.add('ListeJeux');
+
+   const img = document.createElement('img');
+   img.classList.add('photo1');
+   img.src = '/assets/horizon.jpg';
+  
+
+   const video = document.createElement('video');
+   video.classList.add('video1');
+   video.src = '/assets/horizon1.webm';
+   video.loop = true;
+   video.muted = true;
+
+   div.appendChild(img);
+   div.appendChild(video);
+
+   jeuxdumoment?.appendChild(div);
+ }
+ }
 }
+
+
+
+
+
+
