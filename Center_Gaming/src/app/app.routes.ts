e242dafa80ab41b1660e2d6ad9a64ac60c1053cb
alim-ben <you@example.com>
2024-04-16 13:33:40 +0200

    Page Categories

--- Center_Gaming/src/app/app.routes.ts
+++ Center_Gaming/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import PageAcceuilComponent from './page-acceuil/page-acceuil.component';
+import { PageCategoriesComponent } from './page-categories/page-categories.component';
 
 export const routes: Routes = [
-    {path : "" , component : PageAcceuilComponent}
+    {path : "" , component : PageAcceuilComponent},
+    {path: "categories", component: PageCategoriesComponent}
 ];
---
+++ Center_Gaming/src/app/page-categories/page-categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-categories.component.html',
+  styleUrl: './page-categories.component.css'
+})
+export class PageCategoriesComponent {
+
+}
