2d6c4c4ede35e3908c81577ddefa1731f0d4c450
maxenceferiau <oursa59170@gmail.com>
2024-04-22 15:00:55 +0200

    acceuile en cour

--- Center_Gaming/src/app/page-acceuil/page-acceuil.component.ts
+++ Center_Gaming/src/app/page-acceuil/page-acceuil.component.ts
@@ -12,19 +12,20 @@ import { Router } from '@angular/router';
 export default class PageAcceuilComponent {
 
   liste_jeux = [...JEUX].splice(0, 9)
-  liste_Promo = [...JEUX].splice (7, 12)
+  liste_Promo = [...JEUX].splice (14, 20)
   constructor(private router:Router){}
 
  /*  mouseenter(photo: HTMLImageElement , prix : HTMLElement, video?: HTMLVideoElement ) */
-  mouseenter(photo: HTMLImageElement , prix : HTMLElement, video?: HTMLVideoElement )
+  mouseenter(photo: HTMLImageElement , prix? : HTMLElement, video?: HTMLVideoElement )
 
   {
 
-
-    if (video && photo && prix) {
-      photo.style.opacity = "0";
+    if(prix)
       prix.style.opacity = "0";
-      video.style.scale = "1.075"
+    if (video && photo) {
+      photo.style.opacity = "0";
+      video.style.scale = "1.075";
+      
 
 
       video.play();
@@ -32,9 +33,11 @@ export default class PageAcceuilComponent {
       console.error("Error:!");
     }
   }
-    mouseleave(photo: HTMLImageElement, prix : HTMLElement, video?: HTMLVideoElement)
+    mouseleave(photo: HTMLImageElement, prix? : HTMLElement, video?: HTMLVideoElement)
     {
       photo.style.opacity= "";
+    if(prix)
+
       prix.style.opacity = "";
       if(video){
         video.style.scale = "";
