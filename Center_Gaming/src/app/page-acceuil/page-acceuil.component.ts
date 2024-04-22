ef205e3e38f8d8ef8bcf5a6f89ad474ee6e033c7
maxenceferiau <oursa59170@gmail.com>
2024-04-22 15:49:53 +0200

    acceuile en cour

--- Center_Gaming/src/app/page-acceuil/page-acceuil.component.ts
+++ Center_Gaming/src/app/page-acceuil/page-acceuil.component.ts
@@ -13,6 +13,7 @@ export default class PageAcceuilComponent {
 
   liste_jeux = [...JEUX].splice(0, 9)
   liste_Promo = [...JEUX].splice (14, 20)
+  liste_Sortie = [...JEUX].splice(20,26)
   constructor(private router:Router){}
 
  /*  mouseenter(photo: HTMLImageElement , prix : HTMLElement, video?: HTMLVideoElement ) */
@@ -23,6 +24,7 @@ export default class PageAcceuilComponent {
     if(prix)
       prix.style.opacity = "0";
     if (video && photo) {
+      
       photo.style.opacity = "0";
       video.style.scale = "1.075";
       
