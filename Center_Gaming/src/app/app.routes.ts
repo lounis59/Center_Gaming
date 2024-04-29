973b4739085469dd030ab449efb3f14f3dd6f82f
maxenceferiau <oursa59170@gmail.com>
2024-04-29 16:23:41 +0200

    Page panier

--- Center_Gaming/src/app/app.routes.ts
+++ Center_Gaming/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import PageAcceuilComponent from './page-acceuil/page-acceuil.component';
 import { PageCategoriesComponent } from './page-categories/page-categories.component';
 import { PageJeuxComponent } from './page-jeux/page-jeux.component';
 import { PageActionComponent } from './page-action/page-action.component';
-
+import { PagePanierComponent } from './page-panier/page-panier.component';
 export const routes: Routes = [
     {path : "" , component : PageAcceuilComponent},
     {path: "categories", component: PageCategoriesComponent},
     {path: "page-jeux/:id",component: PageJeuxComponent},
-    {path: "action", component: PageActionComponent}
+    {path: "action", component: PageActionComponent},
+    {path: "Panier", component: PagePanierComponent},
 ];
