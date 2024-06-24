ea07251ab4d54239234eddd5bedeed4a75996cb6
Lounis59 <you@example.com>
2024-06-24 15:46:55 +0200

    maj inscription

--- Center_Gaming/src/app/page-inscription/page-inscription.component.ts
+++ Center_Gaming/src/app/page-inscription/page-inscription.component.ts
@@ -1,18 +1,64 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-page-inscription',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './page-inscription.component.html',
   styleUrl: './page-inscription.component.css'
 })
 export class PageInscriptionComponent {
   private url = "http://localhost:8085/user";
+  errorEmail = "";
+  errorPassword = "";
+  errorPasswordBis = "";
   
   constructor(){
-    fetch(this.url).then()
+    
+  }
+  submit(form:HTMLFormElement){
+
+    console.log(this.url);
+    this.errorEmail = "";
+    this.errorPassword = "";
+    this.errorPasswordBis = "";
+    const formData = new FormData(form);
+    fetch(this.url,{
+      method:"POST",
+      body:formData
+    }).then((response)=>{
+      
+        response.json().then((data)=>{
+          if(data.status === 201)
+          {
+
+          }
+          else
+          {
+            console.log(data.data.violations);
+            
+            for (const error of data.data.violations) 
+            {
+              switch(error.propertyPath)
+              {
+                case "email": 
+                  this.errorEmail = error.message;
+                  break;
+                case "password":
+                  this.errorPassword = error.message;
+                  break;
+                case "passwordBis":
+                  this.errorPasswordBis = error.message;
+              }
+                
+            }
+          }
+        })
+      
+    }).catch(e=>{console.log(e);
+    })
   }
 
 }
--- Center_Gaming_Api/controller/userController.php
+++ Center_Gaming_Api/controller/userController.php
@@ -1,95 +1,87 @@
-<?php 
+<?php
 // Quels sont les méthodes acceptés par cette page :
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
 session_start();
 
-require __DIR__."/../model/userModel.php";
-require __DIR__."/../service/_csrf.php";
+require __DIR__ . "/../model/userModel.php";
+require __DIR__ . "/../service/_csrf.php";
 
 $regexPass = "/^(?=.*[!?@#$%^&*+-])(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{6,}$/";
 /* 
     Pour une même adresse, selon la méthode utilisé pour la requête, 
     J'obtiendrais différents résultats.
 */
-switch($_SERVER["REQUEST_METHOD"])
-{
-    case "POST":  create(); break;
-    case "GET": read(); break;
-    case "PUT": update(); break;
-    case "DELETE": delete(); break;
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "POST":
+        create();
+        break;
+    case "GET":
+        read();
+        break;
+    case "PUT":
+        update();
+        break;
+    case "DELETE":
+        delete();
+        break;
 }
 
 function create(): void
 {
     // Pour récupérer des données JSON envoyé dans le corps de la requête :
-    $json = file_get_contents("php://input");
-    $data = json_decode($json);
+    // $json = file_get_contents("php://input");
+    // $data = json_decode($json);
 
     $username = $email = $password = "";
     $error = setError();
 
-    if(!empty($data))
-    {
+    if (!empty($_POST)) {
         // traitement username :
-        if(empty($data->username))
-        {
-            setError("username", "Veuillez saisir un nom d'utilisateur");
-        }
-        else
-        {
-            $username = cleanData($data->username);
-            if(!preg_match("/^[a-zA-Z'\s-]{2,25}$/", $username))
-            {
-                setError("username", "Veuillez saisir un nom d'utilisateur valide");
-            }
-        }
+        // if(empty($data->username))
+        // {
+        //     setError("username", "Veuillez saisir un nom d'utilisateur");
+        // }
+        // else
+        // {
+        //     $username = cleanData($data->username);
+        //     if(!preg_match("/^[a-zA-Z'\s-]{2,25}$/", $username))
+        //     {
+        //         setError("username", "Veuillez saisir un nom d'utilisateur valide");
+        //     }
+        // }
         // traitement email :
-        if(empty($data->email))
-        {
+        if (empty($_POST["email"])) {
             setError("email", "Veuillez saisir un email");
-        }
-        else
-        {
-            $email = cleanData($data->email);
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-            {
+        } else {
+            $email = cleanData($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 setError("email", "Veuillez saisir un email valide");
             }
             $resultat = getOneUserByEmail($email);
-            if($resultat)
-            {
+            if ($resultat) {
                 setError("email", "Cet email est déjà enregistré");
             }
         }
         // traitement du mot de passe :
-        if(empty($data->password))
-        {
+        if (empty($_POST["password"])) {
             setError("password", "Veuillez saisir un mot de passe");
-        }
-        else
-        {
-            $password = trim($data->password);
+        } else {
+            $password = trim($_POST["password"]);
             global $regexPass;
-            if(!preg_match($regexPass, $password))
-            {
+            if (!preg_match($regexPass, $password)) {
                 setError("password", "Veuillez saisir un mot de passe plus complexe");
-            }else
-            {
+            } else {
                 $password = password_hash($password, PASSWORD_DEFAULT);
             }
         }
         // traitement confirmation mdp
-        if(empty($data->passwordBis))
-        {
+        if (empty($_POST["passwordBis"])) {
             setError("passwordBis", "Veuillez saisir à nouveau votre mot de passe");
-        }
-        elseif($data->password != $data->passwordBis)
-        {
+        } elseif ($_POST["password"] != $_POST["passwordBis"]) {
             setError("passwordBis", "Veuillez saisir le même mot de passe");
         }
         $error = setError();
-        if(empty($error["violations"]))
-        {
+        if (empty($error["violations"])) {
             addUser($username, $email, $password);
             sendResponse([], 201, "Inscription Validé");
         }
@@ -98,20 +90,16 @@ function create(): void
 }
 function read(): void
 {
-    if(empty($_GET["id"]))
-    {
+    if (empty($_GET["id"])) {
         $users = getAllUsers();
-    }
-    else
-    {
+    } else {
         $users = getOneUserById((int)$_GET["id"]);
     }
     sendResponse($users, 200, "Utilisateur(s) récupéré(s)");
 }
 function update(): void
 {
-    if(!isset($_GET["id"], $_SESSION["idUser"]) || $_SESSION["idUser"] != $_GET["id"])
-    {
+    if (!isset($_GET["id"], $_SESSION["idUser"]) || $_SESSION["idUser"] != $_GET["id"]) {
         sendResponse([], 401, "Accès Interdit !");
     }
     $user = getOneUserById((int)$_GET["id"]);
@@ -122,64 +110,47 @@ function update(): void
     $username = $password = $email = "";
     $error = setError();
 
-    if(!empty($data))
-    {
+    if (!empty($data)) {
         // traitement username :
-        if(empty($data->username))
-        {
+        if (empty($data->username)) {
             $username = $user["username"];
-        }
-        else
-        {
+        } else {
             $username = cleanData($data->username);
-            if(!preg_match("/^[a-zA-Z'\s-]{2,25}$/", $username))
-            {
+            if (!preg_match("/^[a-zA-Z'\s-]{2,25}$/", $username)) {
                 setError("username", "Votre nom d'utilisateur contient des caractères interdits");
             }
         }
         // traitement email :
-        if(empty($data->email) || $data->email == $user["email"])
-        {
+        if (empty($_POST["email"]) || $_POST["email"] == $user["email"]) {
             $email = $user["email"];
-        }
-        else
-        {
+        } else {
             $email = cleanData($data->email);
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-            {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 setError("email", "Veuillez entrer un email valide");
             }
             $exist = getOneUserByEmail($email);
-            if($exist)
-            {
+            if ($exist) {
                 setError("email", "Cet email est déjà enregistré");
             }
         }
         // traitement mdp
-        if(empty($data->password))
-        {
+        if (empty($data->password)) {
             $password = $user["password"];
-        }
-        else
-        {
-            if(empty($data->passwordBis))
-            {
+        } else {
+            if (empty($data->passwordBis)) {
                 setError("passwordBis", "Veuillez saisir à nouveau votre mot de passe");
-            }elseif($data->password != $data->passwordBis)
-            {
+            } elseif ($data->password != $data->passwordBis) {
                 setError("passwordBis", "Veuillez saisir à nouveau votre mot de passe");
             }
             $password = trim($data->password);
             global $regexPass;
-            if(!preg_match($regexPass, $password))
-            {
+            if (!preg_match($regexPass, $password)) {
                 setError("password", "Veuillez saisir un mot de passe plus complexe");
             }
             $password = password_hash($password, PASSWORD_DEFAULT);
         }
         $error = setError();
-        if(empty($error["violations"]))
-        {
+        if (empty($error["violations"])) {
             updateUserById($username, $email, $password, $user["idUser"]);
             sendResponse([], 204, "Utilisateur mis à jour");
         }
@@ -189,17 +160,15 @@ function update(): void
 }
 function delete(): void
 {
-    if(!isset($_GET["id"], $_SESSION["idUser"]) || $_SESSION["idUser"] != $_GET["id"])
-    {
+    if (!isset($_GET["id"], $_SESSION["idUser"]) || $_SESSION["idUser"] != $_GET["id"]) {
         sendResponse([], 401, "Accès Interdit !");
     }
-    
+
     deleteUserById((int)$_GET["id"]);
 
     unset($_SESSION);
     session_destroy();
-    setcookie("PHPSESSID", "", time()-3600);
+    setcookie("PHPSESSID", "", time() - 3600);
 
     sendResponse([], 204, "Compte supprimé et déconnecté");
 }
-?>
